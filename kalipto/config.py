c1a4da9fb0f5d302e5bab3be144a50d4a24d4972
Roberto Reale <rober.reale@gmail.com>
2018-02-12 17:34:36 +0100

    Add config.py.

---
+++ kalipto/config.py
@@ -0,0 +1,4 @@
+access_key = "963083429964402689-GZaJbjfJagq0cFpaheFJKwtWsC8Gb9F"
+access_secret = "rIrD38KIeEewai7fy4JVdknlYFaXlYLNtctaqIaImzMgd"
+consumer_key = "hUu6VraqqjSGHOcKJSLJXORvr"
+consumer_secret = "yPAFJDbaRLN0pXzEAIOCISCKVuNwh9CKQXLHsOMWOVjpeHNuzo"
