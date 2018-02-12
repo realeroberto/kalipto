acfaf30acc5d9511ff81bc40d40dbdf82b7084bf
Roberto Reale <rober.reale@gmail.com>
2018-02-12 17:34:20 +0100

    Integrate gossipy module.

--- kalipto/cmdline.py
+++ kalipto/cmdline.py
@@ -1,13 +1,12 @@
 import random
 import string
-import socket
-import threading
 import time
 import runpy
 import wget
 
 from twitter import *
 from stegano import lsb
+from gossipy import gossipy
 
 
 #-----------------------------------------------------------------------
@@ -28,48 +27,46 @@ twitter_upload = Twitter(domain='upload.twitter.com',
 #-----------------------------------------------------------------------
 # post a new status
 #-----------------------------------------------------------------------
-def send():
-    while True:
-        msg = input('\nLOCAL > ')
-        if msg and len(msg) > 0:
-            secret = lsb.hide("example.png", msg)
-            secret.save("example-steg.png")
-            with open("example-steg.png", "rb") as imagefile:
-                imagedata = imagefile.read()
-            id_img = twitter_upload.media.upload(media = imagedata, media_category = "tweet_image")["media_id_string"]
-            results = twitter.statuses.update(media_ids = id_img)
+def send(msg):
+    secret = lsb.hide("example.png", msg)
+    secret.save("example-steg.png")
+    with open("example-steg.png", "rb") as imagefile:
+        imagedata = imagefile.read()
+    id_img = twitter_upload.media.upload(media = imagedata, media_category = "tweet_image")["media_id_string"]
+    results = twitter.statuses.update(media_ids = id_img)
 
 #-----------------------------------------------------------------------
 # read new statuses
 #-----------------------------------------------------------------------
 def receive():
     since_id = None
-    while True:
-        if not since_id:
-            statuses = twitter.statuses.home_timeline(count = 10)
-        else:
-            statuses = twitter.statuses.home_timeline(since_id = since_id)
+    if not since_id:
+        statuses = twitter.statuses.home_timeline(count = 10)
+    else:
+        statuses = twitter.statuses.home_timeline(since_id = since_id)
 
-        media_files = set()
-        for status in statuses:
-            media = status['entities'].get('media', [])
-            if(len(media) > 0):
-                media_files.add(media[0]['media_url'])
-            #text = status["text"]
-            #since_id = status["id"]
+    media_files = set()
+    for status in statuses:
+        media = status['entities'].get('media', [])
+        if(len(media) > 0):
+            media_files.add(media[0]['media_url'])
+        #text = status["text"]
+        #since_id = status["id"]
 
-        for media_file in media_files:
-            filename = wget.download(media_file, bar=None)
-            text = lsb.reveal(filename)
-            print('\nREMOTE > ' + text)
-
-        time.sleep(60)
+    for media_file in media_files:
+        filename = wget.download(media_file, bar=None)
+        text = lsb.reveal(filename)
+        return text
 
 #-----------------------------------------------------------------------
 # main loop
 #-----------------------------------------------------------------------
 def main():
-    thread_send = threading.Thread(target = send)
-    thread_receive = threading.Thread(target = receive)
-    thread_send.start()
-    thread_receive.start()
+    chat = gossipy.gossipy(send, receive)
+    chat.start()
+
+
+if __name__ == "__main__":
+    main()
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
